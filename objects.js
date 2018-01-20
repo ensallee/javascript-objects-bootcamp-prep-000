0f78b79d58bfe332265f1541729cc05ee01dc6ff
Betsy Sallee <ensallee@gmail.com>
2018-01-20 20:57:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist['Phil Ochs']="Here's to the State of Mississippi";
   return playlist;
 }
+
+function removeFromPlaylist(playlist, artistName){
+  
+}
