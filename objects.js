229304ad3c9eb7be60536c629e6ec897dde3decf
Betsy Sallee <ensallee@gmail.com>
2018-01-20 20:56:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {Whitney:'No Woman'};
 
 function updatePlaylist(playlist, artistName, songTitle){
-  Object.assign({}, {'Phil Ochs': "Here's to the State of Mississippi"});
+  playlist['Phil Ochs']="Here's to the State of Mississippi";
   return playlist;
 }
